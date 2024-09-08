b904e4c8d4bec73664badbcc089843d3281c02fb
Adeniran <adeniraneniola2001@gmail.com>
2024-09-08 18:19:34 +0100

    Added new features and fixed bugs

---
+++ JS/main.js
@@ -0,0 +1,61 @@
+const chatInput = document.querySelector("#chat-input");
+const sendButton = document.querySelector("#chat-icon");
+const chatContainer = document.querySelector(".chat-container");
+
+
+
+let userText = null;
+
+const createElement = (html, className) => {
+    // Create new div and apply chat, specified class and set html content of div
+
+    const chatDiv = document.createElement("div");
+    chatDiv.classList.add("chat", className);
+    chatDiv.innerHTML = html;
+    return chatDiv; //Return the created chat div
+}
+
+const showTypingAnimation = () => {
+    const html =
+        `<div class="chat-content">
+          <div class="chat-details">
+            <img src="./assets/Wagmi-AI.png" alt="chatbot-img" />
+            <div class = "typing-animation">
+
+            <div class="typing-dot" style = "--delay: 0.2s"></div>
+            <div class="typing-dot" style = "--delay: 0.3s"></div>
+            <div class="typing-dot" style = "--delay: 0.4s"></div>
+            </div>
+          </div>
+          <i class="fa-regular fa-clipboard"></i>
+        </div>
+      </div>`;
+
+    // Create an outgoing chat div with user's message and appends it to the chat container
+    const outgoingChatDiv = createElement(html, "incoming");
+    chatContainer.appendChild(outgoingChatDiv);
+}
+
+const handleOutgoingChat = () => {
+    userText = chatInput.value.trim(); //Get chatInput value and removes extra spaces
+
+    const html =
+        `<div class="chat-content">
+            <div class="chat-details">
+                <img src="./assets/user-regular.svg" alt="" />
+                <p>
+                    ${userText}
+                </p>
+            </div>
+        </div>`;
+
+    // Create an outgoing chat div with user's message and appends it to the chat container
+    const outgoingChatDiv = createElement(html, "outgoing");
+    chatContainer.appendChild(outgoingChatDiv);
+    setTimeout(showTypingAnimation, 500);
+    // console.log(userText);
+}
+
+sendButton.addEventListener("click", handleOutgoingChat);
+
+
